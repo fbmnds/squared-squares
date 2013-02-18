122eefffd4ba9350df5ef81a83c0a96188a00698
fbmnds <fbmnds7.gmail.com>
2013-02-18 20:32:00 +0100

    init test cases

--- test/squared_squares/core_test.clj
+++ test/squared_squares/core_test.clj
@@ -2,6 +2,38 @@
   (:use clojure.test
         squared-squares.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest squared-squares-test
+  (testing "squared-squares-test failed."
+
+    (is (= (sqsq 2 2) ["2"]))
+
+    (is (= (sqsq 2 4)
+           [" 2 "
+            "* 4"
+            " * "]))
+
+    (is (= (sqsq 3 81)
+           [" 3 "
+            "1 9"
+            " 8 "]))
+
+    (is (= (sqsq 4 20)
+           [" 4 "
+            "* 1"
+            " 6 "]))
+
+    (is (= (sqsq 2 256)
+           ["  6  "
+            " 5 * "
+            "2 2 *"
+            " 6 4 "
+            "  1  "]))
+
+    (is (= (sqsq 10 10000)
+           ["   0   "
+            "  1 0  "
+            " 0 1 0 "
+            "* 0 0 0"
+            " * 1 * "
+            "  * *  "
+            "   *   "]))))
